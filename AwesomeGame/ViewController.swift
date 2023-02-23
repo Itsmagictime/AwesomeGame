fc63f0ec635b98bd6ecc420b65405870fc487b0a
Artur Zainulov <artur.zainulov@gmail.com>
2023-02-23 13:53:48 +0600

    Initial Commit

---
+++ AwesomeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeGame
+//
+//  Created by Элина Вишневская🍒 on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
